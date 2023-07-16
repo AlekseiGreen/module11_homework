37e6b934d911386007c52c7b174ba6637e51205e
AlekseiGreen <120323580+AlekseiGreen@users.noreply.github.com>
2023-07-16 23:16:29 +0300

    Delete module_10 directory

--- module_10/task_1/script.js
+++
@@ -1,4 +0,0 @@
-document.getElementById('btn-ID').onclick = function() {
-    document.querySelector('.img1').classList.toggle('img-1');
-    document.querySelector('.img2').classList.toggle('img-2');
-}
--- module_10/task_2/script.js
+++
@@ -1,5 +0,0 @@
-document.getElementById('btn-ID').onclick = function() {
-    let W = document.documentElement.clientWidth;
-    let H = document.documentElement.clientHeight;
-    alert('Разрешение экрана = ' + W + 'x' + H);
-}
